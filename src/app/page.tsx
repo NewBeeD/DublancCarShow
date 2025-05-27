f4bc75a87f50d8ec6d57e82540052f2ab480efad
Danphil Daniel <danieldanphil@gmail.com>
2025-05-27 19:00:45 -0400

    Update: One section closed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
         
         <ScheduleHighlight />
-        <WhyAttend />
+        {/* <WhyAttend /> */}
         {/* <FeaturedAttractions /> */}
         <Testimonials />
         <VendorHighlights />
