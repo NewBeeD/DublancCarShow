47cfecff8738301e57e968a3e216bd122208d474
Danphil Daniel <danieldanphil@gmail.com>
2025-05-27 23:46:59 -0400

    Update: VendorHighlights

--- src/Components/Sections/SectionSix/VendorHighlights.tsx
+++ src/Components/Sections/SectionSix/VendorHighlights.tsx
@@ -1,65 +1,45 @@
-import {
-  AppBar,
-  Toolbar,
-  Container,
-  Grid,
-  Card,
-  CardContent,
-  Typography,
-  Button,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  ListItemIcon,
-  Avatar,
-  TextField,
-  Box,
-  Link
-} from '@mui/material';
-import {
-  Schedule as ScheduleIcon,
-  LocationOn as LocationIcon,
-  EmojiEvents,
-  DirectionsCar,
-  VolumeUp,
-  EventAvailable,
-  Instagram,
-  Facebook,
-  Twitter
-} from '@mui/icons-material';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
+import { Container, Typography, Box, Button, Avatar, Stack } from '@mui/material';
 
 const VendorHighlights = () => {
   return (
-      <Container sx={{ py: 8 }}>
-    <Typography variant="h4" align="center" gutterBottom sx={{ fontWeight: 'bold', mb: 6 }}>
-      Featured Brands
-    </Typography>
-    <Grid container spacing={4} justifyContent="center">
-      {['alpine', 'pioneer', 'jbl', 'sony'].map((brand) => (
-        <Grid item key={brand}>
+    <Container sx={{ py: 8 }}>
+      <Typography variant="h4" align="center" gutterBottom sx={{ fontWeight: 'bold', mb: 6 }}>
+        Featured Brands
+      </Typography>
+      <Stack
+        direction="row"
+        spacing={4}
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        useFlexGap
+        sx={{
+          gap: { xs: 4, md: 8 },
+          padding: { xs: 2, md: 0 }
+        }}
+      >
+        {['alpine', 'pioneer', 'jbl', 'sony'].map((brand) => (
           <Avatar
+            key={brand}
             src={`/${brand}-logo.png`}
-            sx={{ width: 120, height: 120, bgcolor: 'white', p: 2 }}
+            sx={{ 
+              width: 120, 
+              height: 120, 
+              bgcolor: 'white', 
+              p: 2,
+              m: 1 // Add margin for better spacing on wrap
+            }}
             variant="square"
           />
-        </Grid>
-      ))}
-    </Grid>
-    <Box sx={{ textAlign: 'center', mt: 4 }}>
-      <Button variant="outlined" color="warning" size="large">
-        Become an Exhibitor
-      </Button>
-    </Box>
-  </Container>
+        ))}
+      </Stack>
+      <Box sx={{ textAlign: 'center', mt: 4 }}>
+        <Button variant="outlined" color="warning" size="large">
+          Become an Exhibitor
+        </Button>
+      </Box>
+    </Container>
   );
 }
 
 export default VendorHighlights;
-
-
-
